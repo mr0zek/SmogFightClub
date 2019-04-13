e232f0b8eab31fef777528c462495a114c17abfe
mr0zek <szymon.szylhabel@gmail.com>
2019-04-13 13:03:04 +0200

    appveyor integration

---
+++ src/SFC.Tests/BDD/HtmlReportConfig.cs
@@ -0,0 +1,43 @@
+﻿using TestStack.BDDfy.Reporters.Html;
+
+namespace SFC.Tests.BDD
+{
+  /// <summary>
+  /// This overrides the default html report setting
+  /// </summary>
+  public class HtmlReportConfig : DefaultHtmlReportConfiguration
+  {
+    /// <summary>
+    /// Change the output file name
+    /// </summary>
+    public override string OutputFileName
+    {
+      get
+      {
+        return "SFC.Tests.html";
+      }
+    }
+
+    /// <summary>
+    /// Change the report header to your project
+    /// </summary>
+    public override string ReportHeader
+    {
+      get
+      {
+        return "Smog Fight Club";
+      }
+    }
+
+    /// <summary>
+    /// Change the report description
+    /// </summary>
+    public override string ReportDescription
+    {
+      get
+      {
+        return "Here comes report of executed stories agains Smog Fight Club system";
+      }
+    }    
+  }
+}
---
+++ src/SFC.Tests/BDD/NewUserRegistration.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac;
+using Microsoft.Extensions.Configuration;
+using RestEase;
+using SFC.Infrastructure;
+using TestStack.BDDfy;
+using Xunit;
+
+namespace SFC.Tests.BDD
+{
+  [Story(
+    AsA = "As an not registered user",
+    IWant = "I want to register account",
+    SoThat = "So that I can receive smog alerts")]
+  public class NewUserRegistration
+  {
+    private const string _url = "http://localhost:5000";
+    private PostAccountModel _postAccountModel;
+
+    [Given]
+    void GivenSystemWithNotRegisteredAccount()
+    {
+      var confBuilder = new ConfigurationBuilder()
+        .AddJsonFile("appSettings.json");
+      var configuration = confBuilder.Build();
+      var connectionString = configuration["ConnectionStrings:DefaultConnection"];
+
+      DbMigrations.Run(connectionString);
+
+      Bootstrap.Run(new string[0], builder =>
+      {
+        builder.RegisterType<TestSmtpClient>().AsImplementedInterfaces();
+      });
+
+      _postAccountModel = new PostAccountModel()
+      {
+        LoginName = Guid.NewGuid().ToString(),
+        Password = Guid.NewGuid().ToString(),
+        ZipCode = "12-234",
+        Email = "ala.ma.kotowska@gmail.com"
+      };
+    }
+
+    async void WhenUserPostRegistrationForm()
+    {
+      string confirmationId = await RestClient.For<IAccountsApi>(_url).PostAccount(_postAccountModel);
+    }
+
+    void ThenSystemSendsConfirmationEmail()
+    {
+      Assert.Single(TestSmtpClient.SentEmails);
+    }
+
+    [Fact]
+    public void Execute()
+    {
+      this.Given(s => s.GivenSystemWithNotRegisteredAccount())
+        .When(s => s.WhenUserPostRegistrationForm())
+        .Then(s=>ThenSystemSendsConfirmationEmail())
+        .BDDfy();      
+    }
+  }
+}
