2cc162978ead5892f7e5578fc52036de3893c7c3
Szymon Szylhabel <szymon.szylhabel@gmail.com>
2023-10-14 13:44:03 +0200

    diagrams fix

---
+++ src/SFC.GiosGateway/Features/SynchronizeReadings.cs
@@ -0,0 +1,14 @@
+﻿using SFC.Infrastructure.Interfaces.Communication;
+using SFC.Infrastructure.Interfaces.TimeDependency;
+
+namespace SFC.GiosGateway.Features
+{
+  [Crontab("* */0 * * * ")]
+  public class SynchronizeReadings : IEventHandler<TimeEvent>
+  {
+    public void Handle(TimeEvent @event)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
--- src/SFC.Infrastructure.Interfaces/TimeDependency/CronIntervalAttribute.cs
+++ src/SFC.Infrastructure.Interfaces/TimeDependency/CronIntervalAttribute.cs
@@ -18,8 +18,9 @@ namespace SFC.Infrastructure.Interfaces.TimeDependency
   /// <br/>Examples:
   /// <br/>1. */5 * * * * - At every 5th minute
   /// <br/>2. 1 * * * *   - At minute 1
-  /// <br/>3. 1-3 * * * * - At every minute from 1 through 3
-  /// <br/>4. 0 0 */1 * * - At 00:00 on every day-of-month
+  /// <br/>3. 0 0 1 * *   - At 00:00 on day-of-month 1.
+  /// <br/>4. 1-3 * * * * - At every minute from 1 through 3
+  /// <br/>5. 0 0 */1 * * - At 00:00 on every day-of-month
   /// </para>
   /// </summary>
   public class CrontabAttribute : Attribute
